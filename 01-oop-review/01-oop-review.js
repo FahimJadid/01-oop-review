f447ce2986f2878d3cc65d6cd1ac944274e45c19
Fahim Al Jadid <b170606023@pharm.jnu.ac.bd>
2023-05-17 23:54:20 +0600

    added javaScript oop review

--- 01-oop-review/01-oop-review.js
+++ 01-oop-review/01-oop-review.js
@@ -468,3 +468,44 @@ const paidUser1 = new paidUserCreator('Alyssa', 8, 25);
 
 paidUser1, increaseBalance();
 paidUser1.sayName();
+
+
+// Subclassing in solution 4
+// ES2015 Class Approach
+
+class userCreator {
+  constructor(name, score){
+    this.name = name;
+    this.score = score;
+  }
+  sayName(){
+    console.log('I am ' + this.name);
+  }
+  increment(){
+    this.score++
+  }
+
+}
+
+
+const user1 = new userCreator('Phil', 5);
+const user2 = new userCreator('Tim', 4);
+
+user1.sayName();
+
+class paidUserCreator extends userCreator {
+  constructor(paidName, paidScore, accountBalance){
+    super(paidName, paidScore)
+    this.accountBalance = accountBalance
+  }
+  increaseBalance(){
+    this.accountBalance++;
+  }
+}
+
+
+const paidUser1 = new paidUserCreator("Alyssa", 8, 25);
+
+paidUser1.increaseBalance();
+paidUser1.sayName();
+
